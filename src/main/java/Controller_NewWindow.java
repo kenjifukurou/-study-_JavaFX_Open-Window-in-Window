aab3f44af475483a2308016f28facc64440f26e9
Kenji Fukurou <59118789+kenjifukurou@users.noreply.github.com>
2021-03-08 20:54:58 +0800

    Add files via upload

---
+++ src/main/java/Controller_NewWindow.java
@@ -0,0 +1,40 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+
+public class Controller_NewWindow {
+
+    @FXML
+    private Button newWindowButton;
+
+    @FXML
+    void handleOpenNewWindow(ActionEvent event) {
+        System.out.println("Open Button is clicked!");
+
+        try {
+            URL fxmlURL = getClass().getResource("second-window.fxml");
+            FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Second Window");
+            stage.initStyle(StageStyle.TRANSPARENT);
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (Exception e) {
+            System.out.println("error loading fxml file");
+        }
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("new-window.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        System.out.println("Main run second");
+    }
+}
